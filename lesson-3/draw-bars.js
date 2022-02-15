01e4be5e3b9dd5e77fd93c36362460c8a007375e
Denis Sanchez <desanchezca@hotmail.com>
2022-02-14 21:59:35 -0500

    Completed histogram chart!

--- lesson-3/draw-bars.js
+++ lesson-3/draw-bars.js
@@ -1,7 +1,18 @@
-async function drawBars() {
+const metrics = [
+  "windSpeed",
+  "moonPhase",
+  "dewPoint",
+  "humidity",
+  "uvIndex",
+  "windBearing",
+  "temperatureMin",
+  "temperatureMax",
+]
+
+async function drawHistogram(metric) {
   const dataset = await d3.json("./assets/my_weather_data.json");
 
-  const metricAccessor = dp => dp.humidity;
+  const metricAccessor = dp => dp[metric];
 
   const width = 600;
 
@@ -42,9 +53,69 @@ async function drawBars() {
   const yAccessor = dp => dp.length;
 
   const yScale = d3.scaleLinear()
-    .domain([0, d3.max[bins, yAccessor]])
+    .domain([0, d3.max(bins, yAccessor)])
     .range([dimensions.boundedHeight, 0])
     .nice();
+
+  const binsGroups = bounds.append("g");
+
+  const binGroups = binsGroups.selectAll("g")
+    .data(bins)
+    .enter()
+      .append("g");
+  
+  const barPadding = 1;
+
+  const barRects = binGroups.append("rect")
+    .attr("x", d => xScale(d.x0) + barPadding / 2)
+    .attr("y", d => yScale(yAccessor(d)))
+    .attr("width", d => d3.max([0, xScale(d.x1) - xScale(d.x0) - barPadding]))
+    .attr("height", d => dimensions.boundedHeight - yScale(yAccessor(d)))
+    .attr("fill", "cornflowerblue")
+
+  const barText = binGroups.filter(yAccessor)
+    .append("text")
+      .attr("x", d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+      .attr("y", d => yScale(yAccessor(d)) - 5)
+      .text(yAccessor)
+        .style("text-anchor", "middle")
+        .attr("fill", "darkgrey")
+        .style("font-size", "12px")
+        .style("font-family", "sans-serif");
+
+  const mean = d3.mean(dataset, metricAccessor);
+
+  const meanLine = bounds.append("line")
+    .attr("x1", xScale(mean))
+    .attr("x2", xScale(mean))
+    .attr("y1", -15)
+    .attr("y2", dimensions.boundedHeight)
+    .attr("stroke", "darkred")
+    .attr("stroke-dasharray", "8px 4px");
+
+  const meanLabel = bounds.append("text")
+    .attr("x", xScale(mean))
+    .attr("y", -20)
+    .text("mean")
+    .attr("fill", "darkred")
+    .style("font-size", "12px")
+    .style("text-anchor", "middle");
+
+  const xAxisGenerator = d3.axisBottom()
+    .scale(xScale);
+
+  const xAxis = bounds.append("g")
+    .call(xAxisGenerator)
+      .style("transform", `translateY(${dimensions.boundedHeight}px)`);
+
+  const xAxisLabel = xAxis.append("text")
+    .attr("x", dimensions.boundedWidth / 2)
+    .attr("y", dimensions.margin.bottom - 10)
+    .attr("fill", "black")
+    .style("font-size", "1.4em")
+    .text(metric)
+    .style("text-transform", "capitalize")
+
 }
 
-drawBars();
+metrics.forEach(drawHistogram);
